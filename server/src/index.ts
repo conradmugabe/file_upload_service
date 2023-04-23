742ad7e4d1424535a050436e815165a302bdfff3
conrad mugabe <conradmugabe@gmail.com>
2023-04-23 19:58:09 +0300

    feat: added cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,13 +15,44 @@ const storageService = new AwsS3Service({
   credentials: { accessKeyId, secretAccessKey },
 });
 
+app.use((res, req: any, next) => {
+  console.log(
+    `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`
+  );
+
+  res.on('finish', () => {
+    console.log(
+      `METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`
+    );
+  });
+
+  next();
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Request-With, Content-Type, Accept, Authorization'
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
 app.get('/', async (req, res) => {
   res.send('Hello World!');
 });
 
 app.post('/', async (req, res) => {
-  const contentType = 'jpeg';
-  const userId = '12';
+  const { contentType, userId } = req.body;
 
   const { signedUrl, key } = await storageService.getSignedUrl({
     bucketName,
